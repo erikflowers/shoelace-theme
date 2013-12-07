da8ad7ee5b641a2d08264ec2c2dfef7b9b73dc39
Erik Flowers <erik@helloerik.com>
2013-12-07 11:37:43 -0700

    Commit of the hybrid-base untouched files

---
+++ loop-error.php
@@ -0,0 +1,11 @@
+	<article <?php hybrid_post_attributes(); ?>>
+
+		<header class="entry-header">
+			<h1 class="entry-title"><?php _e( 'Nothing found', 'hybrid-base' ); ?></h1>
+		</header>
+
+		<div class="entry-content">
+			<p><?php _e( 'Apologies, but no entries were found.', 'hybrid-base' ); ?></p>
+		</div><!-- .entry-content -->
+
+	</article><!-- .hentry .error -->
